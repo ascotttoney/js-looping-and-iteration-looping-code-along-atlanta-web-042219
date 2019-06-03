460e0e9b709e14ffbdd2ddf4cb031e50e16ccbd5
ascotttoney <ascotttoney@gmail.com>
2019-06-03 19:14:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function writeCards(names, event) {
+  let cards = [];
   for (let i = 0; i < names.length; i++) {
-    return `Thank you, ${names[i]}, for the wonderful ${event} gift.`;
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards;
+}
+
+function countdown(num) {
+  let countdown = num;
+  while (countdown >= 0) {
+    console.log(countdown--);
   }
 }
